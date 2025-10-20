5833dc939057d6d3f8b2a1f0dc4434e461349cd3
Jack High <droLcitsacraS@gmail.com>
2025-10-20 11:03:41 -0400

    linked the start screen

--- Assets/Scripts/GuiScripts/PreStreamButtonUI.cs
+++ Assets/Scripts/GuiScripts/PreStreamButtonUI.cs
@@ -4,9 +4,16 @@ using UnityEngine.SceneManagement;
 public class PreStreamButtonUI : MonoBehaviour
 {
     [SerializeField] private string selectScene = "SelectScene";
+    [SerializeField] private string asylumScene = "AsylumLevel2";
 
     public void BackButton()
     {
         SceneManager.LoadScene(selectScene);
     }
+
+    public void StartStreamButton()
+    {
+        SceneManager.LoadScene(asylumScene);   
+    }
+
 }
