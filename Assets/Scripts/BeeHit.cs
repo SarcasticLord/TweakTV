173ff450062f5cf309719a9c455df31d4f868291
hdp7 <hdp7@pct.edu>
2025-10-22 15:24:37 -0400

    Work on Bee Attack

--- Assets/Scripts/BeeHit.cs
+++ Assets/Scripts/BeeHit.cs
@@ -3,19 +3,21 @@ using System.Collections;
 
 public class BugCollision : MonoBehaviour
 {
-    public float disableDuration = 2f; // Time in seconds to disable the chase
+    public float disableDuration = 5f; // Time in seconds to disable the chase
     public float attackDuration = 1f;
     private BeeHealth beeHealth;
     private BeeChase chaseScript;
     public GameObject beeAttack;
     public GameObject hitParticle;
     private Transform self;
+    private Rigidbody rb;
     public float offset;
 
     void Start()
     {
         chaseScript = GetComponent<BeeChase>();
         beeHealth = GetComponent<BeeHealth>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -32,8 +34,6 @@ public class BugCollision : MonoBehaviour
             Debug.Log("Player is within attack range!");
             chaseScript.enabled = false;
             Attack(attackDuration);
-            // You can disable the chase script here if needed
-            // GetComponent<YourChaseScript>().enabled = false;
         }
     }
 
@@ -47,11 +47,14 @@ public class BugCollision : MonoBehaviour
     }
     public void Attack(float cooldown)
     {
+        rb.linearVelocity = new Vector3(0, 0, 0);
+        rb.useGravity = false;
+        Debug.Log("Attacking");
         Vector3 spawnPosition = self.transform.position + self.transform.forward * offset;
         GameObject spawnedHitbox = GameObject.Instantiate(beeAttack, spawnPosition, Quaternion.identity);
-        spawnedHitbox.transform.SetParent(self.transform, true);
         Cooldown(cooldown);
-        
+
+
         //Vector3 spawnPosition = new Vector3(0f,0f,0f);
         //GameObject spawnedHitbox = GameObject.Instantiate(data.hitbox, spawnPosition, Quaternion.identity);
         //spawnedHitbox.transform.SetParent(worldObject.transform, true);
@@ -72,7 +75,25 @@ public class BugCollision : MonoBehaviour
             chaseScript.enabled = false;
             yield return new WaitForSeconds(duration);
             chaseScript.enabled = true;
+            
             Debug.Log("Chase re-enabled.");
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Attack(attackDuration);
+            Debug.Log("Continuing Attack");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            chaseScript.enabled = true;
+            rb.useGravity = true;
         }
     }
 }
