e630528fdf81a1bb062ce8a4fdcde1392d499dbd
hdp7 <hdp7@pct.edu>
2025-10-27 14:02:05 -0400

    Health

---
+++ Assets/Scripts/PlayerHealthDisplay.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerHealthDisplay : MonoBehaviour
+{
+
+    public float spacing = 10f; // Fixed spacing between items
+    public bool stackFromBottom = true;
+
+    void UpdateLayout()
+    {
+        int count = transform.childCount;
+        for (int i = 0; i < count; i++)
+        {
+            RectTransform child = transform.GetChild(i).GetComponent<RectTransform>();
+            if (child == null) continue;
+
+            float yOffset = (child.sizeDelta.y + spacing) * i;
+            if (stackFromBottom)
+            {
+                child.anchoredPosition = new Vector2(0, yOffset);
+            }
+            else
+            {
+                child.anchoredPosition = new Vector2(0, -yOffset);
+            }
+        }
+    }
+
+    void Update()
+    {
+        UpdateLayout();
+    }
+
+    // Optional: Call UpdateLayout() whenever items are added/removed
+}
+
+
