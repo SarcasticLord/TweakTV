43d3f7ccf2615eee264b546211145722c219aca8
hdp7 <hdp7@pct.edu>
2025-11-19 13:02:05 -0500

    door spawn

--- Assets/Scripts/LevelScripts/ExitLevel.cs
+++ Assets/Scripts/LevelScripts/ExitLevel.cs
@@ -1,17 +1,45 @@
 using EasyPeasyFirstPersonController;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitGame : MonoBehaviour
 { 
     private GameObject weapons;
     private GameObject hud;
+    private Scene currentScene;
+    public GameObject exitPrefab;
+    private Transform spawnPointsContainer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
     void Start()
     {
+        currentScene = SceneManager.GetActiveScene();
         weapons = GameObject.FindGameObjectWithTag("Hotbar");
         hud = GameObject.FindGameObjectWithTag("HUD");
+
+        if (spawnPointsContainer == null)
+        {
+            Debug.LogError("No spawn points container assigned.");
+            return;
+        }
+        int childCount = spawnPointsContainer.childCount;
+
+        if (childCount == 0)
+        {
+            Debug.LogError("No spawn points found under the parent container!");
+            return;
+        }
+
+        // Pick a random spawn point
+        int randomIndex = Random.Range(0, childCount);
+        Transform chosenSpawn = spawnPointsContainer.GetChild(randomIndex);
+
+        // Instantiate the door at the chosen spawn point
+        Instantiate(exitPrefab, chosenSpawn.position, chosenSpawn.rotation);
     }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("WholePlayer"))
@@ -30,8 +58,16 @@ public class ExitGame : MonoBehaviour
     {
         if (other.CompareTag("WholePlayer"))
         {
-            other.transform.position = transform.position;
-            other.transform.rotation = Quaternion.Euler(0f, 270f, 0f);
+            if (currentScene.name == "subway")
+            {
+                other.transform.position = transform.position;
+                other.transform.rotation = Quaternion.Euler(0f, 270f, 0f);
+            }
+            if (currentScene.name == "subway")
+            {
+                other.transform.position = transform.position;
+                other.transform.rotation = Quaternion.Euler(0f, 270f, 0f);
+            }
         }
     }
 
